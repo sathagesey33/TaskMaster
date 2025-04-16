3217abbfabeafd23ce58e7a8af3882ebb2622a16
nokekip <kipnoke@gmail.com>
2025-04-16 18:17:19 +0300

    Refactor user model import to use get_user_model for better compatibility

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
+
+User = get_user_model()
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
